0d151b1aff62ae8855c1b8fddc1d2d52e190aea6
MayaMeir <76560531+MayaMeir@users.noreply.github.com>
2020-12-28 11:39:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+var addBtn = document.querySelector('#add_btn');
+var deletebtn = document.querySelector('#remove_all');
+
+function AddFunc() {
+    // adding item to list
+    var item = document.getElementById("myInput").value,
+        list = document.querySelector('ul'),
+        li = document.createElement('li'),
+        li_data = document.createTextNode(item);
+    checkbox = document.createElement('input');
+    checkbox.classList.add("checkbox")
+    checkbox.setAttribute('type', 'checkbox');
+    li.appendChild(checkbox);
+    li.appendChild(li_data);
+    list.appendChild(li);
+    // adding delete btn
+    var delbtn = document.createElement('button'),
+        delbtn_data = document.createTextNode('X');
+    delbtn.appendChild(delbtn_data);
+    delbtn.classList.add('del_btn');
+    li.appendChild(delbtn);
+
+    delbtn.onclick = function () {
+        li.remove();
+    }
+}
+
+function DelFunc(){
+    var boxes = document.getElementsByClassName('checkbox');
+    var list_items = document.getElementsByTagName('li');
+    for(var i = 0; i<boxes.length;){
+        box = boxes[i];
+        list_item = list_items[i];
+        if(box.checked){
+            box.parentElement.remove();
+        }
+        else i++;
+    }
+}
+
+
+deletebtn.addEventListener('click', DelFunc);
+addBtn.addEventListener('click', AddFunc);
